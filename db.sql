92e38ce72fe92967831ad33f2273a84cc6e7e8c6
CharuBohra <charubohra2004@gmail.com>
2026-06-12 18:51:32 +0530

    Added Values in Products Table

---
+++ db.sql
@@ -0,0 +1,11 @@
+INSERT into PRODUCTS VALUES (001 , "Bonus Saver", "Savings" , 2.00, 5000, null);
+INSERT into PRODUCTS VALUES (002 , "SA1", "Savings" , 2.50, 5000, null);
+INSERT into PRODUCTS VALUES (003 , "FD1", "Fixed Deposits" , 2.00, 5000, 12);
+INSERT into PRODUCTS VALUES (004 , "FD2", "Fixed Deposits" , 3.00, 6000, 36);
+INSERT into PRODUCTS VALUES (005 , "LA1", "Limited Access" , 5.00, 5000, null);
+INSERT into PRODUCTS VALUES (006 , "SA2", "Savings" , 3.00, 6000, null);
+INSERT into PRODUCTS VALUES (007 , "LA2", "Limited Access" , 5.00, 5000, null);
+INSERT into PRODUCTS VALUES (008 , "FD3", "Fixed Deposits" , 3.50, 7000, 24);
+INSERT into PRODUCTS VALUES (009 , "LA3", "Limited Access" , 2.00, 5000, null);
+INSERT into PRODUCTS VALUES (010 , "SA3", "Savings" , 6.00, 4500, null);
+INSERT into PRODUCTS VALUES (011 , "FD4", "Fixed Deposits" , 4.00, 2500, 60);
