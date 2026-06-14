925f28650c8c49e2ea8ad73595cbf7af5d6d1f5b
Pokala Rahul <pokala.rahul@osbgroup.co.uk>
2026-06-14 19:00:05 +0100

    Added admin role handling in CLI login flow

--- src/main/java/com/bank/cli/display/MenuDisplay.java
+++ src/main/java/com/bank/cli/display/MenuDisplay.java
@@ -1,5 +1,7 @@
 package com.bank.cli.display;
 
+import com.bank.enums.Role;
+
 import java.util.Scanner;
 
 /**
@@ -167,8 +169,14 @@ public class MenuDisplay {
         String password = scanner.nextLine().trim();
         
         // TODO: Call AuthService to validate credentials
-        // TODO: If successful, branch on role: showCustomerMenu() or showAdminMenu()
+
+        if (user.getRole() == Role.ADMIN) {
+            showAdminMenu();
+        } else {
+            showCustomerMenu();
+        }
         System.out.println("TODO: Implement login logic using AuthService");
+
     }
     
     private void handleCreateProfile() {
