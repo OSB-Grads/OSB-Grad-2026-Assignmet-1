2e77a2e7a711ef04ac27977a649097740183d49a
Ammar Hasan <ammar.hasan@osbgroup.co.uk>
2026-06-14 23:00:00 +0530

    42 - Implemented Inbox Service getTopMessage

---
+++ src/main/java/com/bank/customer/InboxService.java
@@ -0,0 +1,27 @@
+package com.bank.customer;
+
+import com.bank.db.repository.InboxRepository;
+import com.bank.dto.InboxDTO;
+import com.bank.exception.DatabaseOperationException;
+import com.bank.mapper.InboxMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InboxService {
+
+    private final InboxRepository inboxRepository;
+
+    public InboxService(InboxRepository inboxRepository) {
+        this.inboxRepository = new InboxRepository();
+    }
+
+    public InboxDTO getTopMessage() {
+        try{
+            Map<String, Object> messageList = inboxRepository.findFirst();
+            return InboxMapper.toDTO(messageList);
+        } catch (Exception e) {
+            throw new DatabaseOperationException("Failed to get the message",e);
+        }
+    }
+}
