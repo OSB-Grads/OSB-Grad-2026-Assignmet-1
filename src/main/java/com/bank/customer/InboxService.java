d96f087aed962898626adff28446462791c76acf
Shivakumar Khot <shivakumar.khot@osbgroup.co.uk>
2026-06-15 12:16:32 +0530

    #43 - added delete method  to Inbox service

---
+++ src/main/java/com/bank/customer/InboxService.java
@@ -0,0 +1,18 @@
+package com.bank.customer;
+
+import com.bank.db.repository.InboxRepository;
+
+public class InboxService {
+    private final InboxRepository inboxRepository;
+   public InboxService(){
+        this.inboxRepository=new InboxRepository();
+    }
+
+    public void deleteById(Long id){
+        try {
+           inboxRepository.deleteById(id);
+        } catch (Exception e) {
+             System.out.println("Unable to delete");
+        }
+    }
+}
