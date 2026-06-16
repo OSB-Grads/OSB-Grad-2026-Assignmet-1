a89c2c1f1bca0174c77706bf92adfcd5251ebdb1
ammarhasan6679 <123263921+ammarhasan6679@users.noreply.github.com>
2026-06-16 18:43:50 +0530

    #69 - Created logs for getAllAccountsForCustomer (#81)
    Co-authored-by: Ammar Hasan <ammar.hasan@osbgroup.co.uk>

--- src/main/java/com/bank/customer/AccountsService.java
+++ src/main/java/com/bank/customer/AccountsService.java
@@ -1,6 +1,8 @@
 package com.bank.customer;
-
 import com.bank.db.repository.AccountRepository;
+import com.bank.enums.log.LogType;
+import com.bank.exception.DatabaseOperationException;
+
 import com.bank.dto.AccountDTO;
 import com.bank.enums.log.LogType;
 import com.bank.mapper.AccountMapper;
@@ -19,15 +21,46 @@ public class AccountsService {
 
     public AccountsService() {
         this.accountRepository = new AccountRepository();
-        this.loggerService=new LoggerService();
+        this.loggerService = new LoggerService();
     }
     public List<Map<String,Object>> getAllAccountsForCustomer(Long customerId) {
-        List<Map<String ,Object>> rows = accountRepository.
-                getAccountsWithProductByCustomerId(customerId);
-        if(rows == null || rows.isEmpty()) {
-            return null;
+        try {
+            List<Map<String ,Object>> rows = accountRepository.
+                    getAccountsWithProductByCustomerId(customerId);
+            if(rows == null || rows.isEmpty()) {
+                loggerService.log(
+                        customerId,
+                        "FETCH_ALL_ACCOUNTS",
+                        "No Accounts found for customer: " + customerId,
+                        LogType.ERROR
+                );
+                return Collections.emptyList();
+            }
+            loggerService.log(customerId,
+                    "FETCH_ALL_ACCOUNTS",
+                    "Fetched all accounts for customer" + customerId,
+                    LogType.SUCCESS
+                    );
+            return rows;
+        } catch (DatabaseOperationException e) {
+            loggerService.log(
+                    customerId,
+                    "FETCH_ALL_ACCOUNTS",
+                    "Failed to fetch all accounts for customer: " + customerId +
+                    ". Reason" + e.getMessage(),
+                    LogType.FAILURE
+            );
+            throw e;
+        } catch (Exception e) {
+            loggerService.log(
+                    customerId,
+                    "FETCH_ALL_ACCOUNTS",
+                    "Failed to fetch all accounts for customer: " + customerId +
+                            ". Reason" + e.getMessage(),
+                    LogType.FAILURE
+            );
+            throw e;
         }
-        return rows;
     }
 
      public Long createAccount(Long customerId,Long productId ) {
