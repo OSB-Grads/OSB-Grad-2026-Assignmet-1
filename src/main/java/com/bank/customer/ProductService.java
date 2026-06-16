5355898d787697d97324ee7612fa6928a477fe60
nitya-naik <149999833+nitya-naik@users.noreply.github.com>
2026-06-16 18:44:21 +0530

    #36 - Updated ProductService (#78)
    * feature-#36:Added getAllAccountsByProductCategory() to ProductService
    
    * Updated getAccountsByProductCategory
    
    * Updated productService

--- src/main/java/com/bank/customer/ProductService.java
+++ src/main/java/com/bank/customer/ProductService.java
@@ -1,25 +1,67 @@
 package com.bank.customer;
 
+import com.bank.db.repository.AccountRepository;
 import com.bank.db.repository.ProductRepository;
+import com.bank.dto.AccountDTO;
 import com.bank.dto.ProductDTO;
+import com.bank.mapper.AccountMapper;
 import com.bank.enums.log.LogType;
 import com.bank.exception.ProductsNotFoundForCategoryException;
 import com.bank.mapper.ProductMapper;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ProductService {
-     private final ProductRepository repository;
-     private final LoggerService loggerService;
 
-     public ProductService(){
-         this.repository = new ProductRepository();
-         this.loggerService = new LoggerService();
-     }
+    private final ProductRepository productRepository;
+    private final AccountRepository accountRepository;
+    private final LoggerService loggerService;
+
+
+    public ProductService(){
+        this.productRepository = new ProductRepository();
+        this.accountRepository = new AccountRepository();
+        this.loggerService = new LoggerService();
+    }
+
+    public List<ProductDTO> listProductsByCategory(String category) throws SQLException {
+
+        List<Map<String, Object>> allProducts =
+                productRepository.findAllByProductCategory(category);
+
+        return allProducts.stream()
+                .map(ProductMapper::toDTO)
+                .toList();
+    }
+
+    public List<AccountDTO> getAllAccountsForProductCategory(String category)
+            throws SQLException {
+
+        List<Long> productIds = Collections.unmodifiableList(productRepository
+                .findAllByProductCategory(category)
+                .stream()
+                .map(row -> (Long) row.get("id"))
+                .toList());
+
+        List<AccountDTO> accounts = new ArrayList<>();
+
+        for (Long productId : productIds) {
+
+            List<Map<String, Object>> accountRows =
+                    accountRepository.findAccountsByProductId(productId);
+
+            accounts.addAll(
+                    accountRows.stream()
+                            .map(AccountMapper::toDTO)
+                            .toList()
+            );
+        }
+        return accounts;
+    }
 
      public List<ProductDTO> listProductsByCategory(String category) throws SQLException {
          try{
