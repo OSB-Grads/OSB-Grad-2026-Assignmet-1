6079f7654928c9e72430d7d73227897a128b74db
Ammar Hasan <ammar.hasan@osbgroup.co.uk>
2026-06-16 18:08:39 +0530

    #69 - Created logs for getAllAccountsForCustomer

--- src/main/java/com/bank/customer/AccountsService.java
+++ src/main/java/com/bank/customer/AccountsService.java
@@ -1,27 +1,57 @@
 package com.bank.customer;
-
 import com.bank.db.repository.AccountRepository;
-import com.bank.dto.AccountDTO;
-import com.bank.mapper.AccountMapper;
+import com.bank.enums.log.LogType;
+import com.bank.exception.DatabaseOperationException;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class AccountsService {
     private final AccountRepository accountRepository;
+    private final LoggerService loggerService;
 
     public AccountsService() {
         this.accountRepository = new AccountRepository();
+        this.loggerService = new LoggerService();
     }
     public List<Map<String,Object>> getAllAccountsForCustomer(Long customerId) {
-        List<Map<String ,Object>> rows = accountRepository.
-                getAccountWithProductByCustomerId(customerId);
-        if(rows == null || rows.isEmpty()) {
-            return null;
+        try {
+            List<Map<String ,Object>> rows = accountRepository.
+                    getAccountsWithProductByCustomerId(customerId);
+            if(rows == null || rows.isEmpty()) {
+                loggerService.log(
+                        customerId,
+                        "FETCH_ALL_ACCOUNTS",
+                        "No Accounts found for customer: " + customerId,
+                        LogType.ERROR
+                );
+                return Collections.emptyList();
+            }
+            loggerService.log(customerId,
+                    "FETCH_ALL_ACCOUNTS",
+                    "Fetched all accounts for customer" + customerId,
+                    LogType.SUCCESS
+                    );
+            return rows;
+        } catch (DatabaseOperationException e) {
+            loggerService.log(
+                    customerId,
+                    "FETCH_ALL_ACCOUNTS",
+                    "Failed to fetch all accounts for customer: " + customerId +
+                    ". Reason" + e.getMessage(),
+                    LogType.FAILURE
+            );
+            throw e;
+        } catch (Exception e) {
+            loggerService.log(
+                    customerId,
+                    "FETCH_ALL_ACCOUNTS",
+                    "Failed to fetch all accounts for customer: " + customerId +
+                            ". Reason" + e.getMessage(),
+                    LogType.FAILURE
+            );
+            throw e;
         }
-        return rows;
     }
 }
