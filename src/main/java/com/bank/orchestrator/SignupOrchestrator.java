0b0c4ce73e0eccd6c40b222a14d28fe7ac35f731
Pokala Rahul <pokala.rahul@osbgroup.co.uk>
2026-06-22 10:42:24 +0100

    UUId- implemeneted

--- src/main/java/com/bank/orchestrator/SignupOrchestrator.java
+++ src/main/java/com/bank/orchestrator/SignupOrchestrator.java
@@ -30,8 +30,8 @@ public class SignupOrchestrator {
             String phone,
             String address, String nationalId, String password) throws UserCreationFailedException, UserAlreadyExistsException, SQLException {
                 db.startTransaction();
-                Long customerId= authService.signup(username,password);
-                customerService.createCustomer(customerId,firstName,lastName,dateOfBirth,email,phone,address,nationalId);
+                String customerId= authService.signup(username,password);
+                customerService.createCustomer(Long.valueOf(customerId),firstName,lastName,dateOfBirth,email,phone,address,nationalId);
                 db.endTransaction();
         }
     }
